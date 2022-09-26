b9064ab144caf3c09ac0b01e5a5b9f10c9265579
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2022-09-26 23:32:00 +0530

    changes done to code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//import Login from "./Login";
+//import Navigation from "./Navigation";
 import Test from "./Test";
 //import Navigation from "./Navigation";
 //import LeftBar from "./LeftBar";
